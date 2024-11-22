5373f647411de374f44315a4b9f6cb59c55024ee
shefroyrowe <shefroy.rowe@gmail.com>
2024-11-22 15:09:53 -0800

    add img, update styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './styles.css';
-import { greeting } from './greeting.js';
+import greeting from './greeting.js';
+import odinImage from "./odin.png";
 
 //console.log(greeting);
+
 const demo = document.querySelector('.demo');
 demo.textContent = `${greeting}`;
+   
+const image = document.createElement("img");
+image.src = odinImage;
+image.style.cssText = 'width: 30rem; height: 30rem;';
+   
+document.body.appendChild(image);
